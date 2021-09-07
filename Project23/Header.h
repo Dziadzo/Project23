55ac30aa5dcee5d3e9a3381250b299ba7428ab0c
Dziadzo <m.dziadzo@outlook.com>
2021-09-07 11:46:53 +0300

    Добавьте файлы проекта.

---
+++ Project23/Header.h
@@ -0,0 +1,13 @@
+#pragma once
+#pragma once
+#include <stdlib.h>;
+#include <iostream>;
+#include <iomanip>;
+using namespace std;
+
+int** createArray(int m, int n);
+void pushRandomNumber(int** array, int m, int n);
+void printArray(int** array, int m, int n);
+void delArray(int** array, int m, int n);
+void checkArray(int** array, int m, int n);
+
---
+++ Project23/main.cpp
@@ -0,0 +1,12 @@
+#include "Header.h"
+void main() {
+	int m = 0;
+	int n = 0;
+	cin >> m;
+	cin >> n;
+	int** array = createArray(m, n);
+	pushRandomNumber(array, m, n);
+	printArray(array, m, n);
+	checkArray(array, m, n);
+	delArray(array, m, n);
+}
